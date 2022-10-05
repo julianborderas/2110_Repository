fdf84026eeccf5bc1722b49334b59796b9766558
julianborderas <57376149+julianborderas@users.noreply.github.com>
2022-10-05 13:32:15 -0700

    Added assignment 2

---
+++ Assignment_2/Assignment_2/Assignment_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
